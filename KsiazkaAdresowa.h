6ab47b81289627d136bdb67760412ae5656bfa3c
unknown <kinkow31@wp.pl>
2018-11-28 13:25:35 +0100

    Create base Ksiazka Adresowa and Uzytkownik

---
+++ KsiazkaAdresowa.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+#include "Uzytkownik.h"
+
+using namespace std;
+
+class KsiazkaAdresowa
+{
+    int idZalogowanegoUzytkownika;
+    int idOstatniegoAdresata;
+    int idUsunietegoAdresata;
+
+public:
+    void rejestracjaUzytkownika ();
+
+};
